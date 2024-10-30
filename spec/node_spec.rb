6276cd93a90e1c651058081116b4d793fecd5756
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 16:25:24 +0100

    Add tests for implementing core variables

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,13 +2,32 @@ require "rspec"
 require_relative "../lib/node"
 
 RSpec.describe Node do
+  let(:subject) { Node.new(0) }
+
   describe "@left_node" do
+    it "implements a getter" do
+      expect(subject).to respond_to :left_node
+    end
+
+    it "implements a setter" do
+      expect(subject).to respond_to :left_node=
+    end
   end
 
   describe "@right_node" do
+    it "implements a getter" do
+      expect(subject).to respond_to :right_node
+    end
+
+    it "implements a setter" do
+      expect(subject).to respond_to :right_node=
+    end
   end
 
   describe "@data" do
+    it "implements a getter" do
+      expect(subject).to respond_to :data
+    end
   end
 
   describe "#<=>" do
