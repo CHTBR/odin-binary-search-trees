b1b7da184e12dd8d94e0ca2f209acb80ed5cce20
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 16:44:25 +0100

    Add public interface descriptions

---
+++ spec/tree_spec.rb
@@ -0,0 +1,40 @@
+require "rspec"
+require_relative "../lib/tree"
+
+RSpec.describe Tree do
+  describe "#build-tree" do
+  end
+
+  describe "#insert" do
+  end
+
+  describe "#delete" do
+  end
+
+  describe "#find" do
+  end
+
+  describe "#level_order" do
+  end
+
+  describe "#inorder" do
+  end
+
+  describe "#preorder" do
+  end
+
+  describe "#postorder" do
+  end
+
+  describe "#height" do
+  end
+
+  describe "#depth" do
+  end
+
+  describe "#balanced?" do
+  end
+
+  describe "#rebalance" do
+  end
+end
