3e6824cc1fb5d09d4f690eeefb4ed563fc6430fc
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 17:39:55 +0100

    Add tests for #build-tree

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -3,6 +3,23 @@ require_relative "../lib/tree"
 
 RSpec.describe Tree do
   describe "#build-tree" do
+    context "when given an array of values of the same type" do
+      it "returns the root node" do
+        expect(build_tree([3, 4, 5, 1, 2])).to eql 3
+      end
+
+      it "assigns the root node to @root" do
+        tree = subject
+        tree.build_tree([4, 1, 5, 3, 2])
+        expect(tree.root).to eql 3
+      end
+
+      it "creates a balanced tree" do
+        tree = subject
+        tree.build_tree([5, 2, 3, 4, 1])
+        expect()
+      end
+    end
   end
 
   describe "#insert" do
