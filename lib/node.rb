a57b5a2a440b4f0ac081df7229b1b2ac9e9d62f8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 16:36:26 +0100

    Add <=> and Comparable mixin to make it easier to compare Nodes

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,15 @@
 # A class for storing basic information needed for a tree and handling comparisons
 class Node
+  include Comparable
+
   attr_accessor :left_node, :right_node
   attr_reader :data
 
-  def initialize(data); end
+  def initialize(data)
+    @data = data
+  end
 
-  def <=>(other); end
+  def <=>(other)
+    data <=> other.data
+  end
 end
