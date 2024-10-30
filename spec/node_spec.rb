3b11619ea052fc24a21d98ef4fd571c6e88ada43
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 16:19:32 +0100

    Add public interface descriptions

---
+++ spec/node_spec.rb
@@ -0,0 +1,16 @@
+require "rspec"
+require_relative "../lib/node"
+
+RSpec.describe Node do
+  describe "@left_node" do
+  end
+
+  describe "@right_node" do
+  end
+
+  describe "@data" do
+  end
+
+  describe "#<=>" do
+  end
+end
