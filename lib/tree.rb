bad8f1ac82949e214974b53fc2dca9cb6de6d039
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-31 10:19:45 +0100

    Add class and public interface methods

---
+++ lib/tree.rb
@@ -0,0 +1,28 @@
+# A class for managing the creation and traversal of a tree DS
+class Tree
+  def initialize(arr = []); end
+
+  def build_tree(arr); end
+
+  def insert(value); end
+
+  def delete(value); end
+
+  def find(value); end
+
+  def level_order; end
+
+  def inorder; end
+
+  def preorder; end
+
+  def postorder; end
+
+  def height(node); end
+
+  def depth(node); end
+
+  def balanced?; end
+
+  def rebalance; end
+end
